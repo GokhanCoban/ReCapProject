8eb172f3d3c6f57008ed98559fe2baffa26410b0
Gokhan Çoban <Gokhan@DESKTOP-JF5GJPP>
2021-03-04 22:24:29 +0300

    7.Gün Ödev

---
+++ DataAccess/Concrete/InMemoryDal/InMemoryCarDal.cs
@@ -0,0 +1,56 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataAccess.Concrete.InMemoryDal
+{
+    public class InMemoryCarDal : ICarDal
+    {
+        List<Car> _cars;
+
+        public InMemoryCarDal()
+        {
+            _cars = new List<Car>
+            {
+                new Car{CarId=1,BrandId=1,ColorId=1,Description="Aile Arabası",ModelYear=2015,DailyPrice=200},
+                new Car{CarId=2,BrandId=2,ColorId=2,Description="Spor Araba",ModelYear=2018,DailyPrice=300},
+                new Car{CarId=3,BrandId=3,ColorId=3,Description="Hafif Ticari",ModelYear=2010,DailyPrice=100},
+                new Car{CarId=4,BrandId=4,ColorId=1,Description="Şirket Arabası",ModelYear=2007,DailyPrice=50}
+            };
+        }
+
+        public void Add(Car car)
+        {
+            _cars.Add(car);
+        }
+
+        public void Delete(Car car)
+        {
+            Car carToDelete = _cars.SingleOrDefault(c => c.CarId == car.CarId);
+            _cars.Remove(carToDelete);
+        }
+
+        public List<Car> GetAll()
+        {
+            return _cars;
+        }
+
+        public Car GetById(int carId)
+        {
+            return _cars.FirstOrDefault(c=> c.CarId == carId);
+        }
+
+        public void Update(Car car)
+        {
+            Car carToUpdate = _cars.SingleOrDefault(c=> c.CarId==car.CarId);
+            carToUpdate.BrandId = car.BrandId;
+            carToUpdate.ColorId = car.ColorId;
+            carToUpdate.Description = car.Description;
+            carToUpdate.ModelYear = car.ModelYear;
+            carToUpdate.DailyPrice = carToUpdate.DailyPrice;
+        }
+    }
+}
